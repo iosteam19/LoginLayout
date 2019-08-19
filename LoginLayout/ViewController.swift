8df24e9dfe0b6c48c7df074689b32bb6739a66d1
user1 <user1@samosyss-Mac-mini.local>
2019-08-19 18:06:12 +0530

    Initial Commit

---
+++ LoginLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginLayout
+//
+//  Created by user1 on 28/05/1941 Saka.
+//  Copyright © 1941 user1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
